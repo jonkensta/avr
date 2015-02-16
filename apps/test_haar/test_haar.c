967e0b14b15546bf20399c19ba2d7cb2f410e65a
Jonathan Starr <jonstarr@utexas.edu>
2015-02-16 17:34:06 -0600

    Remove CuTest library.

--- apps/test_haar/test_haar.c
+++ apps/test_haar/test_haar.c
@@ -1,44 +1,46 @@
 #include <Arduino.h>
-#include <CuTest.h>
 #include <haar.h>
 
-void
-test_all_ones(CuTest* tc)
+#define __ASSERT_USE_STDERR
+#include <assert.h>
+
+int
+test_all_ones(void)
 {
     int8_t in[8];
     for (int idx=0; idx<8; idx++)
-    {
         in[idx] = 1;
-    }
 
     int8_t ws[8];
     haar8(in, ws);
 
     int8_t out[8] = {1, 0, 0, 0, 0, 0, 0, 0};
     for (int idx=0; idx<8; idx++)
-    {
-        CuAssertIntEquals(tc, out[idx], in[idx]);
-    }
+        assert(out[idx] == in[idx]);
 }
 
 void
 setup(void)
 {
-    CuString* output = CuStringNew();
-    CuSuite* suite = CuSuiteNew();
-
-    SUITE_ADD_TEST(suite, test_all_ones);
-
-    CuSuiteRun(suite);
-    CuSuiteSummary(suite, output);
-    CuSuiteDetails(suite, output);
-
     Serial.begin(9600);
-    Serial.println(output->buffer);
+    test_all_ones();
+    Serial.println("all tests ran successfully");
 }
 
 void
 loop(void)
 {
     return;
+}
+
+// handle diagnostic informations given by assertion and abort program execution:
+void __assert(const char *__func, const char *__file, int __lineno, const char *__sexp) {
+    // transmit diagnostic informations through serial link.
+    Serial.println(__func);
+    Serial.println(__file);
+    Serial.println(__lineno, DEC);
+    Serial.println(__sexp);
+    Serial.flush();
+    // abort program execution.
+    abort();
 }
