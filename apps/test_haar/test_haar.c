80d415c21f25ca4f403cd31fdf9012e5c79f388f
Jonathan Starr <jonstarr@utexas.edu>
2015-02-16 21:44:39 -0600

    Add more Haar unit tests.

--- apps/test_haar/test_haar.c
+++ apps/test_haar/test_haar.c
@@ -7,10 +7,7 @@
 void
 test_all_ones(void)
 {
-    int8_t in[8];
-    for (int idx=0; idx<8; idx++)
-        in[idx] = 1;
-
+    int8_t in[8] = {1, 1, 1, 1, 1, 1, 1, 1};
     int8_t ws[4];
     haar8(in, ws);
 
@@ -20,10 +17,62 @@ test_all_ones(void)
 }
 
 void
+test_low_freq(void)
+{
+    int8_t in[8] = {1, 1, 1, 1, -1, -1, -1, -1};
+    int8_t ws[4];
+    haar8(in, ws);
+
+    const int8_t out[8] = {0, 1, 0, 0, 0, 0, 0, 0};
+    for (int idx=0; idx<8; idx++)
+        assert(out[idx] == in[idx]);
+}
+
+void
+test_mid_freq(void)
+{
+    int8_t in[8] = {1, 1, -1, -1, 1, 1, -1, -1};
+    int8_t ws[4];
+    haar8(in, ws);
+
+    const int8_t out[8] = {0, 0, 1, 0, 0, 0, 0, 0};
+    for (int idx=0; idx<8; idx++)
+        assert(out[idx] == in[idx]);
+}
+
+void
+test_high_freq(void)
+{
+    int8_t in[8] = {1, -1, -1, 1, -1, 1, 1, -1};
+    int8_t ws[4];
+    haar8(in, ws);
+
+    const int8_t out[8] = {0, 0, 0, 0, 0, 0, 0, 1};
+    for (int idx=0; idx<8; idx++)
+        assert(out[idx] == in[idx]);
+}
+
+void
+test_multi_freq(void)
+{
+    int8_t in[8] = {2, 0, 0, -2, 2, 0, 0, -2};
+    int8_t ws[4];
+    haar8(in, ws);
+
+    const int8_t out[8] = {0, 0, 1, 0, 1, 0, 0, 0};
+    for (int idx=0; idx<8; idx++)
+        assert(out[idx] == in[idx]);
+}
+
+void
 setup(void)
 {
     Serial.begin(9600);
     test_all_ones();
+    test_low_freq();
+    test_mid_freq();
+    test_high_freq();
+    test_multi_freq();
     Serial.println("all tests ran successfully");
 }
 
