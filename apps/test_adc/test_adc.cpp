67470a2f92af0e0797167418198d6c93ef606f51
Jonathan Starr <jonstarr@utexas.edu>
2015-06-17 00:45:44 -0500

    Add AGC classes for more types.

--- apps/test_adc/test_adc.cpp
+++ apps/test_adc/test_adc.cpp
@@ -1,6 +1,7 @@
 #include <string.h>
 
 #include <avr/sleep.h>
+#include <avr/pgmspace.h>
 
 #include <Arduino.h>
 #include <SPI.h>
@@ -35,9 +36,8 @@ const uint8_t SS_PIN = 10;
 const uint8_t SPI_RATE = SPI_CLOCK_DIV2;
 MCP3201 SPI_ADC = MCP3201(SPI_RATE, SS_PIN);
 
-const uint16_t SAMPLER_ATTACK_RATE = 30000;
-const uint8_t SAMPLER_MARGIN = 3;
-ShiftAGC16 SAMPLER_AGC = ShiftAGC16(SAMPLER_ATTACK_RATE, SAMPLER_MARGIN);
+ShiftAGC_I16 SAMPLER_AGC = ShiftAGC_I16(2000, 5);
+ShiftAGC_I16 TEMPORAL_AGC = ShiftAGC_I16(10000, 1000);
 
 volatile int16_t window[FHT_N];
 volatile bool WINDOW_READY = false;
@@ -46,9 +46,11 @@ volatile bool TIME_EVENT = false;
 void capture_sample_callback(void) {
     int16_t sample = SPI_ADC.read();
 
-    bool event = SAMPLER_AGC.update(sample);
-    if (event) TIME_EVENT = true;
+    SAMPLER_AGC.update(sample);
     sample = SAMPLER_AGC.apply(sample);
+
+    bool event = TEMPORAL_AGC.update(sample);
+    if (event) TIME_EVENT = true;
 
     static uint16_t sample_index = 0;
     window[sample_index] = sample;
@@ -94,10 +96,10 @@ color_mode_t next_color_mode(color_mode_t mode) {
     }
 }
 
-void dither_colors(uint8_t* colors, size_t len) {
-    for (int ii=1; ii<len; ii++) {
-        uint8_t lhs = colors[ii-1] >> 1;
-        uint8_t rhs = colors[ii] >> 1;
+void dither_colors(uint8_t* colors) {
+    for (int ii=1; ii<NUM_PIXELS; ii++) {
+        uint8_t lhs = (3 * (uint16_t)colors[ii]) >> 2;
+        uint8_t rhs = (colors[ii-1]) >> 2;
         colors[ii] = lhs + rhs;
     }
 }
@@ -118,16 +120,17 @@ void setup() {
 }
 
 int map_pixel_to_bin(int pixel) {
-    if (pixel <= 32)
-        return pixel + 1;
-    else if (pixel <= (32+16))
-        return 2 * (pixel - (32)) + 32;
-    else if (pixel <= (32+16+8))
-        return 4 * (pixel - (32+16)) + 64;
-    else if (pixel <= (32+16+8+4))
-        return 8 * (pixel - (32+16+8)) + 96;
-    else
-        return 0;
+    return pixel + 1;
+//    if (pixel <= 32)
+//        return pixel + 1;
+//    else if (pixel <= (32+16))
+//        return 2 * (pixel - (32)) + 32;
+//    else if (pixel <= (32+16+8))
+//        return 4 * (pixel - (32+16)) + 64;
+//    else if (pixel <= (32+16+8+4))
+//        return 8 * (pixel - (32+16+8)) + 96;
+//    else
+//        return 0;
 }
 
 void loop() {
@@ -151,20 +154,43 @@ void loop() {
     fht_run(); // process the data in the fht
     fht_mag_lin(); // take the output of the fht
 
-    static uint16_t avg_mag[FHT_N/2];
-    const uint16_t decay = 1;
-    for (int bin=0; bin<FHT_N/2; bin++) {
-        uint16_t lhs = fht_lin_out[bin];
-        uint16_t rhs = avg_mag[bin];
-        uint16_t result = (lhs + ((1 << decay) - 1) * rhs) >> decay;
-        avg_mag[bin] = max(result, lhs);
+    for (int pixel=(3*NUM_PIXELS/4); pixel<NUM_PIXELS; pixel++) {
+        uint16_t bin = map_pixel_to_bin(pixel);
+        fht_lin_out[bin] <<= 3;
+    }
+
+    for (int pixel=(NUM_PIXELS/2); pixel<(3*NUM_PIXELS/4); pixel++) {
+        uint16_t bin = map_pixel_to_bin(pixel);
+        fht_lin_out[bin] <<= 1;
+    }
+
+    static ShiftAGC_U16 AGC[4] = {
+        ShiftAGC_U16(2000, 5),
+        ShiftAGC_U16(2000, 5),
+        ShiftAGC_U16(2000, 5),
+        ShiftAGC_U16(2000, 5),
+    };
+
+    for (int pixel=0; pixel<NUM_PIXELS; pixel++) {
+        int band = pixel / (NUM_PIXELS / 4);
+        uint16_t bin = map_pixel_to_bin(pixel);
+        AGC[band].update(fht_lin_out[bin]);
+        fht_lin_out[bin]= AGC[band].apply(fht_lin_out[bin]);
+    }
+
+    static uint8_t avg_mag[NUM_PIXELS];
+    for (int pixel=0; pixel<NUM_PIXELS; pixel++) {
+        uint16_t bin = map_pixel_to_bin(pixel);
+        uint8_t lhs = fht_lin_out[bin] >> 8;
+        uint8_t rhs = avg_mag[pixel];
+        uint8_t result = (lhs >> 1) + (rhs >> 1);
+        avg_mag[pixel] = max(result, lhs);
     }
 
     for (int pixel=0; pixel<NUM_PIXELS; pixel++) {
-        int bin = map_pixel_to_bin(pixel);
-        uint8_t r = (avg_mag[bin] * colors[0][pixel]) >> 8;
-        uint8_t b = (avg_mag[bin] * colors[1][pixel]) >> 8;
-        uint8_t g = (avg_mag[bin] * colors[2][pixel]) >> 8;
+        uint8_t r = ((uint16_t)avg_mag[pixel] * colors[0][pixel]) >> 8;
+        uint8_t b = ((uint16_t)avg_mag[pixel] * colors[1][pixel]) >> 8;
+        uint8_t g = ((uint16_t)avg_mag[pixel] * colors[2][pixel]) >> 8;
         pixels.setPixelColor(pixel, r, g, b);
     }
     pixels.show();
@@ -177,11 +203,13 @@ void loop() {
     for (int ii=0; ii<3; ii++)
         colors[ii][0] = 0;
 
+    int color_index;
     switch (color_mode) {
-        case RED:   colors[0][0] = COLOR_MAX; break;
-        case BLUE:  colors[1][0] = COLOR_MAX; break;
-        case GREEN: colors[2][0] = COLOR_MAX; break;
+        case RED:   color_index = 0; break;
+        case BLUE:  color_index = 1; break;
+        case GREEN: color_index = 2; break;
     }
+    colors[color_index][0] = COLOR_MAX;
     for (int ii=0; ii<3; ii++)
-        dither_colors(&(colors[ii][0]), NUM_PIXELS);
+        dither_colors(&(colors[ii][0]));
 }
--- libs/AGC/AGC.h
+++ libs/AGC/AGC.h
@@ -3,61 +3,192 @@
 
 #include <inttypes.h>
 
-template <typename T>
-class ShiftAGC {
+class ShiftAGC_I16 {
   public:
     // Constructor
-    ShiftAGC(uint32_t attack_rate, uint8_t margin) 
+    ShiftAGC_I16(uint32_t attack_rate, uint32_t backoff_rate)
         : _attack_rate(attack_rate)
-        , _margin(margin)
+        , _backoff_rate(backoff_rate)
+        , _sizeof_type(8*sizeof(_overflow_level))
     {
+        _overflow_level = 1 << (_sizeof_type - 1);
     }
 
     // Public methods
-    inline bool update(T value)
+    inline bool update(int16_t value)
     {
-        T abs_value = (value >= 0) ? (value) : (-value);
+        uint16_t abs_value = (value >= 0) ? (value) : (-value);
 
-        if (!(abs_value & _mask)) {
+        if (abs_value < _overflow_level) {
             _attack_count++;
 
             if (_attack_count >= _attack_rate) {
                 _attack_count = 0;
-                _shift++;
-                _mask = calculate_mask(_shift, _margin);
-            }
+                _backoff_count = 0;
 
-            return false;
+                if (_shift < (_sizeof_type - 1))
+                    _shift++;
+                _overflow_level = 1 << (_sizeof_type - _shift - 1);
+            }
         }
         else {
-            _shift--;
-            _mask = calculate_mask(_shift, _margin);
+            _backoff_count++;
 
-            return true;
+            if (_backoff_count >= _backoff_rate) {
+                _attack_count = 0;
+                _backoff_count = 0;
+
+                if (_shift > 0)
+                    _shift--;
+                _overflow_level = 1 << (_sizeof_type - _shift - 1);
+
+                return true;
+            }
         }
+
+        return false;
     }
 
-    inline T apply(T value)
+    inline int16_t apply(int16_t value)
     {
         return (value << _shift);
     }
 
   private:
     uint8_t _shift;
-    uint8_t _margin;
-    T _mask;
+    uint16_t _overflow_level;
 
-    uint32_t _attack_rate;
+    const uint8_t _sizeof_type;
+
+    const uint32_t _attack_rate;
     uint32_t _attack_count;
 
-    static inline T calculate_mask(uint8_t shift, uint8_t margin)
-    {
-        return ~((1 << ((sizeof(T) - 1) - (shift + margin))) - 1);
-    }
+    const uint32_t _backoff_rate;
+    uint32_t _backoff_count;
 };
 
-typedef ShiftAGC<int8_t>  ShiftAGC8;
-typedef ShiftAGC<int16_t> ShiftAGC16;
-typedef ShiftAGC<int32_t> ShiftAGC32;
+class ShiftAGC_U16 {
+  public:
+    // Constructor
+    ShiftAGC_U16(uint32_t attack_rate, uint32_t backoff_rate)
+        : _attack_rate(attack_rate)
+        , _backoff_rate(backoff_rate)
+        , _sizeof_type(8*sizeof(_overflow_level))
+    {
+        _overflow_level = 1 << (_sizeof_type - 1);
+    }
+
+    // Public methods
+    inline bool update(uint16_t value)
+    {
+        if (value < _overflow_level) {
+            _attack_count++;
+
+            if (_attack_count >= _attack_rate) {
+                _attack_count = 0;
+                _backoff_count = 0;
+
+                if (_shift < (_sizeof_type - 1))
+                    _shift++;
+                _overflow_level = 1 << (_sizeof_type - _shift - 1);
+            }
+        }
+        else {
+            _backoff_count++;
+
+            if (_backoff_count >= _backoff_rate) {
+                _attack_count = 0;
+                _backoff_count = 0;
+
+                if (_shift > 0)
+                    _shift--;
+                _overflow_level = 1 << (_sizeof_type - _shift - 1);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    inline uint16_t apply(uint16_t value)
+    {
+        return (value << _shift);
+    }
+
+  private:
+    uint8_t _shift;
+    uint16_t _overflow_level;
+
+    const uint8_t _sizeof_type;
+
+    const uint32_t _attack_rate;
+    uint32_t _attack_count;
+
+    const uint32_t _backoff_rate;
+    uint32_t _backoff_count;
+};
+
+class ShiftAGC_U8 {
+  public:
+    // Constructor
+    ShiftAGC_U8(uint32_t attack_rate, uint32_t backoff_rate)
+        : _attack_rate(attack_rate)
+        , _backoff_rate(backoff_rate)
+        , _sizeof_type(8*sizeof(_overflow_level))
+    {
+        _overflow_level = 1 << (_sizeof_type - 1);
+    }
+
+    // Public methods
+    inline bool update(uint8_t value)
+    {
+        if (value < _overflow_level) {
+            _attack_count++;
+
+            if (_attack_count >= _attack_rate) {
+                _attack_count = 0;
+                _backoff_count = 0;
+
+                if (_shift < (_sizeof_type - 1))
+                    _shift++;
+                _overflow_level = 1 << (_sizeof_type - _shift - 1);
+            }
+        }
+        else {
+            _backoff_count++;
+
+            if (_backoff_count >= _backoff_rate) {
+                _attack_count = 0;
+                _backoff_count = 0;
+
+                if (_shift > 0)
+                    _shift--;
+                _overflow_level = 1 << (_sizeof_type - _shift - 1);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    inline uint8_t apply(uint8_t value)
+    {
+        return (value << _shift);
+    }
+
+  private:
+    uint8_t _shift;
+    uint8_t _overflow_level;
+
+    const uint8_t _sizeof_type;
+
+    const uint32_t _attack_rate;
+    uint32_t _attack_count;
+
+    const uint32_t _backoff_rate;
+    uint32_t _backoff_count;
+};
 
 #endif /* #ifndef AGC_H */
