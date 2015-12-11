04e10a41392e8717fce4c36b7ae1eff5af9ee521
Jonathan Starr <jonstarr@utexas.edu>
2015-12-11 12:59:52 -0600

    Various changes for space efficiency

--- apps/test_adc/test_adc.cpp
+++ apps/test_adc/test_adc.cpp
@@ -7,7 +7,7 @@
 #include <TimerOne.h>
 
 #define LIN_OUT 1
-#define FHT_N 256
+#define FHT_N 128
 #include <FHT.h>
 
 #include <neopixel.h>
@@ -15,12 +15,14 @@
 #include <AGC.h>
 #include <ADC/MCP3201.h>
 
+const uint8_t NUM_BINS = FHT_N >> 1;
+
 ////////////
 // Pixels //
 ////////////
 
-#define LED_PIN         4
-#define NUM_PIXELS      60
+const uint8_t LED_PIN = 4;
+const uint16_t NUM_PIXELS = 300;
 Neopixel pixels = Neopixel(NUM_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup_pixels(void) {
@@ -36,11 +38,8 @@ const uint8_t SPI_RATE = SPI_CLOCK_DIV2;
 MCP3201 SPI_ADC = MCP3201(SPI_RATE, SS_PIN);
 
 ShiftAGC_I16 SAMPLER_AGC = ShiftAGC_I16(5000, 5);
-ShiftAGC_I16 TEMPORAL_AGC = ShiftAGC_I16(10000, 1000);
 
-volatile int16_t window[FHT_N];
-volatile bool WINDOW_READY = false;
-volatile bool TIME_EVENT = false;
+volatile bool INPUT_READY = false;
 
 void capture_sample_callback(void) {
     int16_t sample = SPI_ADC.read();
@@ -48,63 +47,46 @@ void capture_sample_callback(void) {
     SAMPLER_AGC.update(sample);
     sample = SAMPLER_AGC.apply(sample);
 
-    bool event = TEMPORAL_AGC.update(sample);
-    if (event) TIME_EVENT = true;
-
     static uint16_t sample_index = 0;
-    window[sample_index] = sample;
+    fht_input[sample_index] = sample;
     sample_index++;
 
     if (sample_index >= FHT_N) {
-        WINDOW_READY = true;
+        INPUT_READY = true;
         sample_index = 0;
         Timer1.stop();
     }
 }
 
 void setup_sampler(void) {
-    const uint16_t sampling_period_us = 200;
+    const uint16_t sampling_period_us = 350;
     Timer1.initialize(sampling_period_us);
     Timer1.attachInterrupt(capture_sample_callback);
     SPI_ADC.begin();
 }
 
-//////////////
-// coloring //
-//////////////
+////////////
+// colors //
+////////////
 
-typedef enum {RED=0, BLUE, GREEN} color_mode_t;
-color_mode_t color_mode = GREEN;
-uint8_t colors[3][NUM_PIXELS];
-const uint8_t COLOR_MAX = 255;
+void next_color_state(uint8_t& r, uint8_t& b, uint8_t& g)
+{
+    static uint8_t _r = 255, _b = 0, _g = 0;
 
-void setup_colors(void) {
-    for (int ii=0; ii<NUM_PIXELS; ii++)
-        colors[0][ii] = COLOR_MAX;
-}
-
-color_mode_t next_color_mode(color_mode_t mode) {
-    switch (mode) {
-        case RED:
-            return BLUE;
-        case BLUE:
-            return GREEN;
-        case GREEN:
-        default:
-            return RED;
+    if (_r > 0 && _g == 0) {
+        _r--;
+        _b++;
+    } else if (_b > 0 && _r == 0) {
+        _b--;
+        _g++;
+    } else {
+        _g--;
+        _r++;
     }
-}
 
-void disperse_colors(uint8_t* colors, const uint8_t boundary) {
-    uint8_t lhs = (3 * (uint16_t)colors[0]) >> 2;
-    uint8_t rhs = boundary >> 2;
-    colors[0] = lhs + rhs;
-
-    for (int ii=1; ii<NUM_PIXELS; ii++) {
-        uint8_t lhs = (3 * (uint16_t)colors[ii]) >> 2;
-        uint8_t rhs = (colors[ii-1]) >> 2;
-        colors[ii] = lhs + rhs;
-    }
+    r = _r;
+    b = _b;
+    g = _g;
 }
 
 ///////////////////////////
@@ -118,17 +100,12 @@ void setup_sleep(void) {
 void setup() {
     setup_pixels();
     setup_sampler();
-    setup_colors();
     setup_sleep();
-}
-
-inline int map_pixel_to_bin(int pixel) {
-    return pixel + 2;
 }
 
 void loop() {
     cli();
-    if (!WINDOW_READY) {
+    if (!INPUT_READY) {
         sleep_enable();
         sei();
         sleep_cpu();
@@ -136,56 +113,50 @@ void loop() {
         return;
     }
 
-    memcpy(fht_input, (const void*)window, sizeof(int16_t) * FHT_N);
-
-    WINDOW_READY = false;
-    Timer1.resume();
-    sei();
-
     fht_window(); // window the data for better frequency response
     fht_reorder(); // reorder the data before doing the fht
     fht_run(); // process the data in the fht
     fht_mag_lin(); // take the output of the fht
 
-    const uint8_t num_bands = 5;
+    const uint8_t num_bands = 8;
     static ShiftAGC_U16 AGC[num_bands] = {
         ShiftAGC_U16(5000, 500),
         ShiftAGC_U16(5000, 500),
         ShiftAGC_U16(5000, 500),
         ShiftAGC_U16(5000, 500),
         ShiftAGC_U16(5000, 500),
+        ShiftAGC_U16(5000, 500),
+        ShiftAGC_U16(5000, 500),
+        ShiftAGC_U16(5000, 500),
     };
-    for (int pixel=0; pixel<NUM_PIXELS; pixel++) {
-        int band = pixel / (NUM_PIXELS / num_bands);
-        uint16_t bin = map_pixel_to_bin(pixel);
+    for (int bin=1; bin<NUM_BINS; bin++) {
+        int band = bin / (NUM_BINS / num_bands);
         AGC[band].update(fht_lin_out[bin]);
-        fht_lin_out[bin]= AGC[band].apply(fht_lin_out[bin]);
+        fht_lin_out[bin] = AGC[band].apply(fht_lin_out[bin]);
     }
 
-    static uint8_t avg_mag[NUM_PIXELS];
-    for (int pixel=0; pixel<NUM_PIXELS; pixel++) {
-        uint16_t bin = map_pixel_to_bin(pixel);
+    static uint8_t avg_mag[NUM_BINS];
+    for (uint8_t bin=0; bin<NUM_BINS; bin++) {
         uint8_t lhs = fht_lin_out[bin] >> 8;
-        uint8_t rhs = avg_mag[pixel];
+        uint8_t rhs = avg_mag[bin];
         uint8_t result = (lhs >> 1) + (rhs >> 1);
-        avg_mag[pixel] = max(result, lhs);
+        avg_mag[bin] = max(result, lhs);
     }
 
-    for (int pixel=0; pixel<NUM_PIXELS; pixel++) {
-        uint8_t r = ((uint16_t)avg_mag[pixel] * colors[0][pixel]) >> 8;
-        uint8_t b = ((uint16_t)avg_mag[pixel] * colors[1][pixel]) >> 8;
-        uint8_t g = ((uint16_t)avg_mag[pixel] * colors[2][pixel]) >> 8;
-        pixels.setPixelColor(pixel, r, g, b);
+    INPUT_READY = false;
+    Timer1.resume();
+    sei();
+
+    uint8_t r, g, b;
+    next_color_state(r, g, b);
+    const uint8_t factor = (NUM_PIXELS + (NUM_BINS >> 1)) / NUM_BINS;
+    for (uint16_t pixel=0; pixel<NUM_PIXELS; pixel++) {
+        uint8_t brightness = avg_mag[pixel / factor + 2];
+        uint8_t _r = ((uint16_t)r * brightness) >> 8;
+        uint8_t _g = ((uint16_t)g * brightness) >> 8;
+        uint8_t _b = ((uint16_t)b * brightness) >> 8;
+        pixels.setPixelColor(pixel, _r, _g, _b);
     }
+
     pixels.show();
-
-    if (TIME_EVENT) {
-        color_mode = next_color_mode(color_mode);
-        TIME_EVENT = false;
-    }
-
-    for (int ii=0; ii<3; ii++) {
-        uint8_t boundary = (ii != color_mode) ? 0 : COLOR_MAX;
-        disperse_colors(&(colors[ii][0]), boundary);
-    }
 }
