f31ae53f40acebffb7386e3a37d2a596f2349e84
Jonathan Starr <jonstarr@utexas.edu>
2017-10-11 23:06:21 -0500

    Simplify transmission calls

--- libs/devices/lib/MCP23017.cpp
+++ libs/devices/lib/MCP23017.cpp
@@ -11,10 +11,6 @@ void MCP23017::write(uint16_t value) {
     Wire.beginTransmission(_address);
     Wire.write(MCP23017_GPIOA);
     Wire.write(value & 0xFF);
-    Wire.endTransmission();
-
-    Wire.beginTransmission(_address);
-    Wire.write(MCP23017_GPIOB);
     Wire.write(value >> 8);
     Wire.endTransmission();
 }
@@ -24,28 +20,22 @@ void MCP23017::write(uint16_t value) {
  */
 void MCP23017::set_pin_mode(uint8_t mode) {
     mode = (mode == INPUT) ? 0xFF : 0x00;
-
     Wire.beginTransmission(_address);
     Wire.write(MCP23017_IODIRA);
     Wire.write(mode);
-    Wire.endTransmission();
-
-    Wire.beginTransmission(_address);
-    Wire.write(MCP23017_IODIRB);
     Wire.write(mode);
     Wire.endTransmission();
 }
 
+/**
+ * Sets the pull-up either on or off
+ */
 void MCP23017::set_pull_up(bool value) {
     uint8_t mode = (value) ? 0xFF : 0x00;
 
     Wire.beginTransmission(_address);
     Wire.write(MCP23017_GPPUA);
     Wire.write(mode);
-    Wire.endTransmission();
-
-    Wire.beginTransmission(_address);
-    Wire.write(MCP23017_GPPUB);
     Wire.write(mode);
     Wire.endTransmission();
 }
